3a8a0d3130b6f68a66898e45db6286fdadbc39b5
AlenaLoik <CrazyBeeLviv@gmail.com>
2020-05-13 21:45:57 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import './App.css';
 import { GoodsList } from './GoodsList';
 import Good from './Good';
+import { getGoods } from './Api';
 
 type AppState = {
   goods: Good[];
   isLoaded: boolean;
 };
 
-const getGoods = () => {
-  return fetch('https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json')
-    .then(response => response.json());
-};
+const FILTERS = { color: 'red', count: 'sort 5', showAll: 'all' };
 
 class App extends React.Component<{}, AppState> {
   state = {
-    goods: [] as Good[],
+    goods: [],
     isLoaded: false,
   };
 
@@ -25,17 +23,17 @@ class App extends React.Component<{}, AppState> {
     });
 
     switch (viewParam) {
-      case 'red':
+      case FILTERS.color:
         getGoods()
-        .then((data) => {
-          this.setState({
-            goods: data.filter((good: Good) => (
-              good.color === 'red'
-            )),
+          .then((data) => {
+            this.setState({
+              goods: data.filter((good: Good) => (
+                good.color === FILTERS.color
+              )),
+            });
           });
-        });
         break;
-      case 'sort 5':
+      case FILTERS.count:
         getGoods()
           .then((data) => {
             this.setState({
@@ -55,24 +53,22 @@ class App extends React.Component<{}, AppState> {
   };
 
   render() {
-    const statuses = ['all', 'sort 5', 'red'];
-
     return (
       <>
         <h1>Dynamic list of Goods</h1>
         <ul className="filters">
-          {statuses.map(status => (
+          {Object.values(FILTERS).map(filter => (
             <li className="filters__button">
-              <a
-                className="filters__link"
-                href={`#/${status}`}
-                onClick={() => (this.filterGoods(status))}
-              >
-                {status}
-              </a>
-            </li>
-          ))}
-        </ul>
+            <a
+              className="filters__link"
+              href={`#/${filter}`}
+              onClick={() => (this.filterGoods(filter))}
+            >
+              {filter}
+            </a>
+          </li>
+        ))}
+      </ul>
         {(this.state.isLoaded)
           ? <GoodsList goods={this.state.goods} />
           : ''}
